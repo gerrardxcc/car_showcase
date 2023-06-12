ac2b9f6389d4075d2230511898781c4ee0b375cb
Gerrard Xiong <chengcheng.xiong@my.jcu.edu.au>
2023-06-12 23:43:39 +1000

    update CarCard and created CarDeatils

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,22 @@
+import { CarProps } from '../types';
+
+export async function fetchCars() {
+  const headers = {
+    'X-RapidAPI-Key': '0742237121msh8ee69650f493a1ep16953ejsne7ca4079e6fd',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+  };
+  const response = await fetch(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=xv',
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
 export const calculateCarRent = (city_mpg: number, year: number) => {
   const basePricePerDay = 50; // Base rental price per day in dollars
   const mileageFactor = 0.1; // Additional rate per mile driven
@@ -13,19 +32,4 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0);
 };
 
-export async function fetchCars() {
-  const headers = {
-    'X-RapidAPI-Key': '0742237121msh8ee69650f493a1ep16953ejsne7ca4079e6fd',
-    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
-  };
-  const response = await fetch(
-    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
-    {
-      headers: headers,
-    }
-  );
-
-  const result = await response.json();
-
-  return result;
-}
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {};
