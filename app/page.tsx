03496e7d94f99d3ccf9087c36f0385115f49dd0e
Gerrard Xiong <chengcheng.xiong@my.jcu.edu.au>
2023-06-18 22:58:26 +1000

    add show more button

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { CarCard, CustomFilter, Hero, SearchBar } from '../components';
 import { fetchCars } from '../utils';
 import { fuels, yearsOfProduction } from '../constants';
+import ShowMore from '../components/ShowMore';
 
 export default async function Home({ searchParams }) {
   const allCars = await fetchCars({
@@ -37,6 +38,11 @@ export default async function Home({ searchParams }) {
                 <CarCard car={car} />
               ))}
             </div>
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className='home__error-container'>
